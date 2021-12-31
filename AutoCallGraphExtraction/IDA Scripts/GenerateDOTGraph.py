492f5824a47109aa24538e375eb2bbcc044d9c78
fnezhadian <nezhadian@gmail.com>
2021-12-31 17:54:50 +0330

    Update GenerateDOTGraph.py

--- AutoCallGraphExtraction/IDA Scripts/GenerateDOTGraph.py
+++ AutoCallGraphExtraction/IDA Scripts/GenerateDOTGraph.py
@@ -1,16 +1,13 @@
 import idaapi
 import idc
 
-def generate_simple_call_chart(start_ea, end_ea, flag):
+
+def generate_simple_call_chart(flag):
     output_path = idc.get_idb_path().rsplit('.')[0]
     title = ''
     wait = ''
     idaapi.gen_simple_call_chart(output_path, wait, title, flag)
   
 
-start_ea = ida_ida.inf_get_min_ea()
-end_ea = ida_ida.inf_get_max_ea()
-
-flag = idaapi.CHART_GEN_DOT
-generate_simple_call_chart(start_ea, end_ea, flag)
+generate_simple_call_chart(idaapi.CHART_GEN_DOT)
 exit()
