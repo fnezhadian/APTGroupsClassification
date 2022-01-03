fe27d4e4833c7c2790aeab82f65f14c0e6e3ba69
fnezhadian <nezhadian@gmail.com>
2022-01-03 12:00:46 +0330

    Update GraphEmbedding.py

--- GraphEmbedding/GraphEmbedding.py
+++ GraphEmbedding/GraphEmbedding.py
@@ -2,9 +2,7 @@ import networkx as nx
 from karateclub.graph_embedding import GL2Vec
 import pydot
 import os
-
-
-dot_files_path = "D:\\Material\\DOT"
+import numpy as np
 
 
 def get_subdirectories(main_dir):
@@ -19,28 +17,45 @@ def get_subdirectories(main_dir):
 def get_files(directory):
     target_files = []
     for filename in os.listdir(directory):
-        full_path = os.path.join(directory, filename)
-        if not os.path.isfile(full_path):
-            continue
-        target_files.append(full_path)
+        if filename.endswith(".dot"):
+            full_path = os.path.join(directory, filename)
+            if not os.path.isfile(full_path):
+                continue
+            target_files.append(full_path)
     return target_files
 
 
 def get_nx_graph(dot_file_path):
-    graphs = pydot.graph_from_dot_file(dot_file_path)
-    converted_graph = nx.nx_pydot.from_pydot(graphs[0])
-    graph = nx.classes.graph.Graph()
+    try:
+        graphs = pydot.graph_from_dot_file(dot_file_path)
+        converted_graph = nx.nx_pydot.from_pydot(graphs[0])
+        graph = nx.classes.graph.Graph()
 
-    # GL2Vec document says: "The procedure assumes that nodes have no string feature present"
-    for n in converted_graph.nodes:
-        if n == '\\n':
-            continue
-        graph.add_node(int(n))
+        # GL2Vec document says: "The procedure assumes that nodes have no string feature present"
+        for n in converted_graph.nodes:
+            if n == '\\n':
+                continue
+            graph.add_node(int(n))
 
-    for e in converted_graph.edges:
-        graph.add_edge(int(e[0]), int(e[1]))
+        for e in converted_graph.edges:
+            graph.add_edge(int(e[0]), int(e[1]))
 
-    return graph
+        return graph
+    except:
+        # TODO: Log
+        print(dot_file_path)
+
+
+def get_graph_list(dot_files_path):
+    graph_list = []
+    target = []
+    for sub_dir in get_subdirectories(dot_files_path):
+        for file_path in get_files(sub_dir):
+            graph_list.append(get_nx_graph(file_path))
+            target.append(os.path.basename(sub_dir))
+            
+    target = np.array(target)
+    return graph_list, target
 
 
 def get_embedding(graph_list):
@@ -50,13 +65,12 @@ def get_embedding(graph_list):
 
 
 def main():
-    graph_list = []
-    for sub_dir in get_subdirectories(dot_files_path):
-        for file_path in get_files(sub_dir):
-            graph_list.append(get_nx_graph(file_path))
-
+    dot_files_path = "D:\\Material\\DOT"
+    graph_list, target = get_graph_list(dot_files_path)
     embedding = get_embedding(graph_list)
-    # TODO: apply machine learning algorithms
+
+    np.savetxt('target.txt', target, fmt='%s')
+    np.savetxt('vector.txt', embedding, fmt='%.18e')
 
 
 if __name__ == "__main__":
