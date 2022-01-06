b9169878e33d7e2693fe4824f248024a76414565
fnezhadian <nezhadian@gmail.com>
2022-01-06 15:48:25 +0330

    Update ApplyClassifiers.py

--- Classification/ApplyClassifiers.py
+++ Classification/ApplyClassifiers.py
@@ -10,51 +10,37 @@ def split_dataset(vectors, target):
     return train_test_split(vectors, target, test_size=0.2)
 
 
-def apply_GaussianNB(X_train, X_test, y_train):
-    classifier = GaussianNB()
+def predict(classifier, X_test):
+    return classifier.predict(X_test)
+
+
+def check_score(classifier, X_test, y_test):
+    return classifier.score(X_test, y_test)
+
+
+def apply_model(classifier, X_train, X_test, y_train, y_test):
     classifier.fit(X_train, y_train)
-    prediction_results = classifier.predict(X_test)
-    score = classifier.score(X_train, y_train)
+    prediction_results = predict(classifier, X_test)
+    score = check_score(classifier, X_test, y_test)
     return prediction_results, score
 
 
-def apply_KNN(X_train, X_test, y_train):
-    classifier = KNeighborsClassifier(n_neighbors=3)
-    classifier.fit(X_train, y_train)
-    prediction_results = classifier.predict(X_test)
-    score = classifier.score(X_train, y_train)
-    return prediction_results, score
-
-
-def apply_KMeans(X_train, X_test, y_train):
-    classifier = KMeans(n_clusters=3, random_state=0)
-    classifier.fit(X_train)
-    prediction_results = classifier.predict(X_test)
-    score = classifier.score(X_train, y_train)
-    return prediction_results, score
-
-
-def apply_RandomForest(X_train, X_test, y_train):
-    classifier = RandomForestClassifier()
-    classifier.fit(X_train, y_train)
-    prediction_results = classifier.predict(X_test)
-    score = classifier.score(X_train, y_train)
-    return prediction_results, score
-
-
-def apply_classifier(vectors, target):
+def apply_classifiers(vectors, target):
     X_train, X_test, y_train, y_test = split_dataset(vectors, target)
-    NB_prediction_results, NB_Score = apply_GaussianNB(X_train, X_test, y_train)
-    KNN_prediction_results, KNN_Score = apply_KNN(X_train, X_test, y_train)
-    KMeans_prediction_results, KMeans_Score = apply_KMeans(X_train, X_test, y_train)
-    RF_prediction_results, RF_Score = apply_RandomForest(X_train, X_test, y_train)
+    NB_classifier = GaussianNB()
+    KNN_classifier = KNeighborsClassifier(n_neighbors=3)
+    RF_classifier = RandomForestClassifier()
+
+    NB_prediction_results, NB_Score = apply_model(NB_classifier, X_train, X_test, y_train, y_test)
+    KNN_prediction_results, KNN_Score = apply_model(KNN_classifier, X_train, X_test, y_train, y_test)
+    RF_prediction_results, RF_Score = apply_model(RF_classifier, X_train, X_test, y_train, y_test)
 
 
 def main():
     target = np.loadtxt('target.txt', dtype=int)
     vectors = np.loadtxt('vector.txt', dtype=float)
-    apply_classifier(vectors, target)
+    apply_classifiers(vectors, target)
 
-
+    
 if __name__ == "__main__":
     main()
