05291cc278dde14cfd4c82ae75fe27e21c3e94a8
fnezhadian <nezhadian@gmail.com>
2022-01-14 16:43:03 +0330

    Update ApplyClassifiers.py

--- Classification/ApplyClassifiers.py
+++ Classification/ApplyClassifiers.py
@@ -1,3 +1,5 @@
+import math
+import os
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
@@ -7,6 +9,7 @@ from sklearn import svm
 from sklearn import tree
 from sklearn.linear_model import LogisticRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neural_network._multilayer_perceptron import MLPClassifier
 from sklearn import metrics
 
 
@@ -36,41 +39,52 @@ def apply_model(classifier, X_train, X_test, y_train, y_test):
     classifier.fit(X_train, y_train)
     prediction_results = predict(classifier, X_test)
     score = get_score(classifier, X_test, y_test)
+    print(type(classifier), round(score, ndigits=2))
+    report = get_classification_report(y_test, prediction_results)
+    print(report)
     return prediction_results, score
 
 
 def apply_classifiers(vectors, target):
     X_train, X_test, y_train, y_test = split_dataset(vectors, target)
-    NB_classifier = GaussianNB()
-    KNN_classifier = KNeighborsClassifier(n_neighbors=50)
-    RF_classifier = RandomForestClassifier()
-    SVM_classifier = svm.SVC()
-    DT_classifier = tree.DecisionTreeClassifier()
-    LR_classifier = LogisticRegression(max_iter=1000, random_state=0)
-    LDA_classifier = LinearDiscriminantAnalysis()
 
-    NB_prediction, NB_Score = apply_model(NB_classifier, X_train, X_test, y_train, y_test)
-    KNN_prediction, KNN_Score = apply_model(KNN_classifier, X_train, X_test, y_train, y_test)
-    RF_prediction, RF_Score = apply_model(RF_classifier, X_train, X_test, y_train, y_test)
-    SVM_prediction, SVM_Score = apply_model(SVM_classifier, X_train, X_test, y_train, y_test)
-    DT_prediction, DT_Score = apply_model(DT_classifier, X_train, X_test, y_train, y_test)
-    LR_prediction, LR_Score = apply_model(LR_classifier, X_train, X_test, y_train, y_test)
-    LDA_prediction, LDA_Score = apply_model(LDA_classifier, X_train, X_test, y_train, y_test)
+    classifiers = [
+        LogisticRegression(max_iter=1000, random_state=0),
+        GaussianNB(),
+        KNeighborsClassifier(n_neighbors=round(math.sqrt(len(X_train)))),
+        tree.DecisionTreeClassifier(),
+        svm.SVC(),
+        # svm.LinearSVC(max_iter=1000),
+        # svm.NuSVC(),
+        svm.SVR(),
+        # svm.LinearSVR(max_iter=1000),
+        # svm.NuSVR(),
+        MLPClassifier(max_iter=1000)
+    ]
 
-    print("GaussianNB: ", NB_Score)
-    print("KNeighbors: ", KNN_Score)
-    print("RandomForest: ", RF_Score)
-    print("SVM: ", SVM_Score)
-    print("DecisionTree: ", DT_Score)
-    print("LogisticRegression: ", LR_Score)
-    print("LinearDiscriminantAnalysis: ", LDA_Score)
+    for classifier in classifiers:
+        prediction, score = apply_model(classifier, X_train, X_test, y_train, y_test)
 
 
 def main():
-    target = np.loadtxt('target.txt', dtype=int)
-    vectors = np.loadtxt('vector.txt', dtype=float)
-    apply_classifiers(vectors, target)
+    datasets_path = [
+        "D:\\Material\\Current\\Dataset\\Call\\FeatherGraph",
+        "D:\\Material\\Current\\Dataset\\Call\\Graph2Vec",
+        "D:\\Material\\Current\\Dataset\\Call\\GL2Vec",
+        "D:\\Material\\Current\\Dataset\\Flow\\FeatherGraph",
+        "D:\\Material\\Current\\Dataset\\Flow\\Graph2Vec",
+        "D:\\Material\\Current\\Dataset\\Flow\\GL2Vec"
+    ]
 
-    
+    for dataset_path in datasets_path:
+        print(dataset_path)
+        target_path = os.path.join(dataset_path, "target.txt")
+        vector_path = os.path.join(dataset_path, "vector.txt")
+
+        target = np.loadtxt(target_path, dtype=int)
+        vectors = np.loadtxt(vector_path, dtype=float)
+        apply_classifiers(vectors, target)
+
+
 if __name__ == "__main__":
     main()
