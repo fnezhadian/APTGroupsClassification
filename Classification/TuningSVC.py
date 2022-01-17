79bd7c2fa4df7f5771f6b3695c470eb89c62eae6
fnezhadian <nezhadian@gmail.com>
2022-01-17 16:32:10 +0330

    Create TuningSVC.py

---
+++ Classification/TuningSVC.py
@@ -0,0 +1,71 @@
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+
+param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001], 'kernel': ['rbf', 'poly', 'sigmoid', 'linear']}
+
+
+def split_dataset(vectors, target):
+    return train_test_split(vectors, target, test_size=0.2)
+
+
+def predict(classifier, X_test):
+    return classifier.predict(X_test)
+
+
+def get_score(classifier, X_test, y_test):
+    return classifier.score(X_test, y_test)
+
+
+def apply_model(classifier, X_train, X_test, y_train, y_test):
+    classifier.fit(X_train, y_train)
+    prediction_results = predict(classifier, X_test)
+    score = get_score(classifier, X_test, y_test)
+    return prediction_results, score
+
+
+def get_fine_tuning(vectors, target):
+    X_train, X_test, y_train, y_test = split_dataset(vectors, target)
+    grid = GridSearchCV(svm.SVC(), param_grid, refit=True, verbose=2)
+    grid.fit(X_train, y_train)
+    grid.score(X_test, y_test)
+    print(grid.best_estimator_)
+    print(grid.best_score_)
+
+
+def apply_classifiers(vectors, target):
+    X_train, X_test, y_train, y_test = split_dataset(vectors, target)
+
+    classifiers = [
+        svm.SVC(kernel='poly', C=100, gamma=0.1),
+        svm.SVC(kernel='rbf', C=100, gamma=0.1),
+        svm.SVC(kernel='sigmoid', C=100, gamma=0.1),
+        svm.SVC(kernel='linear', C=100, gamma=0.1),
+    ]
+
+    for classifier in classifiers:
+        prediction, score = apply_model(classifier, X_train, X_test, y_train, y_test)
+        print(type(classifier), classifier.kernel, round(score, ndigits=2))
+
+
+def main():
+    datasets_path = [
+        "D:\\Material\\Current\\Flow\\Dataset\\GL2Vec",
+    ]
+
+    for dataset_path in datasets_path:
+        print(dataset_path)
+        target_path = os.path.join(dataset_path, "target.txt")
+        vector_path = os.path.join(dataset_path, "vector.txt")
+
+        target = np.loadtxt(target_path, dtype=int)
+        vectors = np.loadtxt(vector_path, dtype=float)
+
+        # get_fine_tuning(vectors, target)  # result: best estimator -> SVC(C = 100, gamma = 0.1)
+        apply_classifiers(vectors, target)  # result: best kernel -> rbf
+
+
+if __name__ == "__main__":
+    main()
